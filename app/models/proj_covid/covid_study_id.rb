c399321f7ad8534d53742ebac2111a2be0e1799c
tibbs001 <sheri.tibbs@duke.edu>
2020-04-29 20:53:48 -0400

    populate the covid table and create the view

--- app/models/proj_covid/covid_study_id.rb
+++ app/models/proj_covid/covid_study_id.rb
@@ -7,14 +7,15 @@ module ProjCovid
       # Retrieve the current list of COVID-related NCT-IDs from ct.gov RSS feed
       file_name = "public/covid_study_ids.csv"
       curl_cmd = "curl 'https://clinicaltrials.gov/ct2/results/rss.xml?rcv_d=&lup_d=&sel_rss=mod14&cond=COVID-19&count=99999' | grep 'link' | cut -c 49-59 > #{file_name}"
-      connection = ActiveRecord::Base.connection.raw_connection
-      connection.exec("COPY proj_covid.covid_study_ids (nct_id) FROM STDIN")
+      system(curl_cmd)
+      conn = ActiveRecord::Base.connection.raw_connection
+      conn.exec("COPY proj_covid.covid_study_ids (nct_id) FROM STDIN")
       data = File.open(file_name)
       data::gets
       data.each { |line|
-        connection.put_copy_data(line + ",,")
+        conn.put_copy_data(line + ",,")
       }
-      connection.put_copy_end
+      conn.put_copy_end
     end
 
   end
